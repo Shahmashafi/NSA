2714d44a1093b79e72c470d0b1fa15aecd967bb5
Shahmashafi <shahmashafi23@gmail.com>
2025-05-06 15:51:30 +0530

    greeting.sh

--- greeting.sh
+++ greeting.sh
@@ -1,8 +1,8 @@
 h=$(date +%H)
-if [ "$h" -ge 5 ] && [ "$h" -lt 12 ]
+if [ "$h" -ge 5 ] && [ "$h" -lt 11]
 then
  echo "GOOD MORNING"
-elif [ "$h" -ge 12 ] && [ "$h" -lt 14 ]
+elif [ "$h" -ge 11 ] && [ "$h" -lt 15 ]
 then
  echo "GOOD AFTERNOON"
 elif [ "$h" -ge 15 ] && [ "$h" -lt 19 ]
